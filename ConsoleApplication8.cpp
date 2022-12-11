ad9ad0241a397aa0f2ef34a803effd15e2eb9635
veronica <laurens.boychuk@gmail.com>
2022-12-11 18:12:07 +0100

    Add project files.

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	struct Computer
+	{
+		string CPU;
+		string Motherboard;
+		string VideoCard;
+		int RAM;
+		int StorageDevice;
+	};
+
+
+	struct Burger
+	{
+		float Cheese;
+		float Meat;
+		float Tomato;
+		float Salat;
+		float Bread;
+	};
+
+
+	struct House
+	{
+		int Floors;
+		int Doors;
+		int Rooms;
+		int Stairs;
+		int Windows;
+	};
+
+
+	struct Coca_Cola
+	{
+		double Sugar;
+		double Wather;
+		string Color;
+		string Bottle;
+		string Aroma;
+	};
+
+
+	struct Person 
+	{
+		string Name;
+		int Age;
+		int Growth;
+		string Country;
+		int Hobby;
+	};
+}
